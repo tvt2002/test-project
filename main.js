bdc0d3863bc22e8619d209cafc1fca8332e67d32
Trieu Thanh <tvthanh.cod@gmail.com>
2024-05-17 01:03:47 +0700

    #1 fixed method call api

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function fetchApi(url) {
     return new Promise(function (resolve, reject) {
         fetch(url)
             .then(function (res) { return resolve(res); })
-            .catch(function () { return reject("can not load data"); });
+            .catch(function () { return reject("can not load data"); })
+            .finally(function () { return console.log("call api finish"); });
     });
 }
